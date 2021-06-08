ed104f2459681cbfc7e1d95ba58a9dac993fe892
SidorovMaxim <maximsidorov1996@gmail.com>
2021-06-08 22:54:53 +0300

    Сreated basic markup

---
+++ src/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <a className='logo' href='/'>
+        Typing Trainer
+      </a>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/main/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <section>
+      <div className='text-container'>
+        
+      </div>
+      <div className='params-container'>
+        
+      </div>
+    </section>
+  );
+}
+
+export default Main;
